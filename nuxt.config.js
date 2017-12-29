7f2943ff3e5111af601e91108ccb933928a031f4
David Gage Hall <davidgage.hall@gmail.com>
2017-12-29 06:30:20 -0500

    changed title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'my_webpage',
+    title: 'David Gage Hall',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
